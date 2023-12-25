2fafb064a318a45f792da185915ba4ee8b977f54
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-12-25 02:46:04 -0500

    no redundant mult-process

--- tools/gen_data/gen_depth_gt.py
+++ tools/gen_data/gen_depth_gt.py
@@ -1,3 +1,4 @@
+from itertools import chain
 import os
 from multiprocessing import Pool
 
@@ -7,6 +8,9 @@ from nuscenes.utils.data_classes import LidarPointCloud
 from nuscenes.utils.geometry_utils import view_points
 from pyquaternion import Quaternion
 import copy
+import pickle
+from tqdm.auto import tqdm
+
 
 # https://github.com/nutonomy/nuscenes-devkit/blob/master/python-sdk/nuscenes/nuscenes.py#L834
 def map_pointcloud_to_image(
@@ -16,14 +20,13 @@ def map_pointcloud_to_image(
     lidar2ego_rotation,
     ego2global_translation,
     ego2global_rotation,
-    sensor2ego_translation, 
+    sensor2ego_translation,
     sensor2ego_rotation,
     cam_ego2global_translation,
     cam_ego2global_rotation,
     cam_intrinsic,
     min_dist: float = 0.0,
 ):
-
     # Points live in the point sensor frame. So they need to be
     # transformed via global to the image plane.
     # First step: transform the pointcloud to the ego vehicle
@@ -53,9 +56,7 @@ def map_pointcloud_to_image(
 
     # Take the actual picture (matrix multiplication with camera-matrix
     # + renormalization).
-    points = view_points(pc.points[:3, :],
-                         cam_intrinsic,
-                         normalize=True)
+    points = view_points(pc.points[:3, :], cam_intrinsic, normalize=True)
 
     # Remove points that are either outside or behind the camera.
     # Leave a margin of 1 pixel for aesthetic reasons. Also make
@@ -74,67 +75,74 @@ def map_pointcloud_to_image(
     return points, coloring
 
 
-data_root = './data/nuscenes'
-info_path_train = './data/nuscenes/nuscenes_occ_infos_train.pkl'
-info_path_val = './data/nuscenes/nuscenes_occ_infos_val.pkl'
+data_root = "./data/nuscenes"
+info_path_train = "./data/nuscenes/nuscenes_occ_infos_train.pkl"
+info_path_val = "./data/nuscenes/nuscenes_occ_infos_val.pkl"
 
 # data3d_nusc = NuscMVDetData()
 
-lidar_key = 'LIDAR_TOP'
+lidar_key = "LIDAR_TOP"
 cam_keys = [
-    'CAM_FRONT_LEFT', 'CAM_FRONT', 'CAM_FRONT_RIGHT', 'CAM_BACK_RIGHT',
-    'CAM_BACK', 'CAM_BACK_LEFT'
+    "CAM_FRONT_LEFT",
+    "CAM_FRONT",
+    "CAM_FRONT_RIGHT",
+    "CAM_BACK_RIGHT",
+    "CAM_BACK",
+    "CAM_BACK_LEFT",
 ]
 
 
 def worker(info):
-    lidar_path = info['lidar_path']
-    points = np.fromfile(lidar_path,
-                         dtype=np.float32,
-                         count=-1).reshape(-1, 5)[..., :4]
-    
-    lidar2ego_translation = info['lidar2ego_translation']
-    lidar2ego_rotation = info['lidar2ego_rotation']
-    ego2global_translation = info['ego2global_translation']
-    ego2global_rotation = info['ego2global_rotation']
+    lidar_path = info["lidar_path"]
+    points = np.fromfile(lidar_path, dtype=np.float32, count=-1).reshape(-1, 5)[..., :4]
+
+    lidar2ego_translation = info["lidar2ego_translation"]
+    lidar2ego_rotation = info["lidar2ego_rotation"]
+    ego2global_translation = info["ego2global_translation"]
+    ego2global_rotation = info["ego2global_rotation"]
     for i, cam_key in enumerate(cam_keys):
-        sensor2ego_translation = info['cams'][cam_key]['sensor2ego_translation']
-        sensor2ego_rotation = info['cams'][cam_key]['sensor2ego_rotation']
-        cam_ego2global_translation = info['cams'][cam_key]['ego2global_translation']
-        cam_ego2global_rotation = info['cams'][cam_key]['ego2global_rotation']
-        cam_intrinsic = info['cams'][cam_key]['cam_intrinsic']
-        img = mmcv.imread(
-            os.path.join(info['cams'][cam_key]['data_path']))
+        sensor2ego_translation = info["cams"][cam_key]["sensor2ego_translation"]
+        sensor2ego_rotation = info["cams"][cam_key]["sensor2ego_rotation"]
+        cam_ego2global_translation = info["cams"][cam_key]["ego2global_translation"]
+        cam_ego2global_rotation = info["cams"][cam_key]["ego2global_rotation"]
+        cam_intrinsic = info["cams"][cam_key]["cam_intrinsic"]
+        img = mmcv.imread(os.path.join(info["cams"][cam_key]["data_path"]))
         pts_img, depth = map_pointcloud_to_image(
-            points.copy(), img, 
-            copy.deepcopy(lidar2ego_translation), 
-            copy.deepcopy(lidar2ego_rotation), 
+            points.copy(),
+            img,
+            copy.deepcopy(lidar2ego_translation),
+            copy.deepcopy(lidar2ego_rotation),
             copy.deepcopy(ego2global_translation),
             copy.deepcopy(ego2global_rotation),
-            copy.deepcopy(sensor2ego_translation), 
-            copy.deepcopy(sensor2ego_rotation), 
-            copy.deepcopy(cam_ego2global_translation), 
+            copy.deepcopy(sensor2ego_translation),
+            copy.deepcopy(sensor2ego_rotation),
+            copy.deepcopy(cam_ego2global_translation),
             copy.deepcopy(cam_ego2global_rotation),
-            copy.deepcopy(cam_intrinsic))
-        
-        file_name = os.path.split(info['cams'][cam_key]['data_path'])[-1]
-        np.concatenate([pts_img[:2, :].T, depth[:, None]],
-                       axis=1).astype(np.float32).flatten().tofile(
-                           os.path.join('./data', 'depth_gt',
-                                        f'{file_name}.bin'))
+            copy.deepcopy(cam_intrinsic),
+        )
 
-if __name__ == '__main__':
-    po = Pool(12)
-    mmcv.mkdir_or_exist(os.path.join('./data', 'depth_gt'))
-    infos = mmcv.load(info_path_train)['infos']
-    for info in infos:
-        po.apply_async(func=worker, args=(info, ))
-    po.close()
-    po.join()
-    
-    po2 = Pool(12)
-    infos = mmcv.load(info_path_val)['infos']
-    for info in infos:
-        po2.apply_async(func=worker, args=(info, ))
-    po2.close()
-    po2.join()
+        file_name = os.path.split(info["cams"][cam_key]["data_path"])[-1]
+        np.concatenate([pts_img[:2, :].T, depth[:, None]], axis=1).astype(
+            np.float32
+        ).flatten().tofile(os.path.join("./data", "depth_gt", f"{file_name}.bin"))
+
+
+if __name__ == "__main__":
+    os.makedirs(os.path.join("./data", "depth_gt"), exist_ok=True)
+    train_infos = pickle.load(open(info_path_train, "rb"))["infos"]
+    val_infos = pickle.load(open(info_path_val, "rb"))["infos"]
+    all_infos = train_infos + val_infos
+    list(map(worker, tqdm(all_infos)))
+    """
+    #numpy is multihreaded, this actually slows down
+    from concurrent.futures import ProcessPoolExecutor, as_completed
+
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        futures = executor.map(worker, all_infos)
+        for f in tqdm(
+            as_completed(futures),
+            total=len(all_infos),
+        ):
+            f.result()
+        executor.shutdown(wait=True)
+    """
