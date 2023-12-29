2eb3a67f5011c83c6d9496f2aa28eab5d7fcea94
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-12-29 09:58:36 -0500

    update

--- setup.py
+++ setup.py
@@ -3,55 +3,53 @@ from setuptools import find_packages, setup
 import os
 import torch
 from os import path as osp
-from torch.utils.cpp_extension import (BuildExtension, CppExtension,
-                                       CUDAExtension)
+from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension
 import torch
 
-def make_cuda_ext(name,
-                  module,
-                  sources,
-                  sources_cuda=[],
-                  extra_args=[],
-                  extra_include_path=[]):
 
+def make_cuda_ext(
+    name, module, sources, sources_cuda=[], extra_args=[], extra_include_path=[]
+):
     define_macros = []
-    extra_compile_args = {'cxx': [] + extra_args}
+    extra_compile_args = {"cxx": [] + extra_args}
 
-    if torch.cuda.is_available() or os.getenv('FORCE_CUDA', '0') == '1':
-        define_macros += [('WITH_CUDA', None)]
+    if torch.cuda.is_available() or os.getenv("FORCE_CUDA", "0") == "1":
+        define_macros += [("WITH_CUDA", None)]
         extension = CUDAExtension
-        extra_compile_args['nvcc'] = extra_args + [
-            '-D__CUDA_NO_HALF_OPERATORS__',
-            '-D__CUDA_NO_HALF_CONVERSIONS__',
-            '-D__CUDA_NO_HALF2_OPERATORS__',
+        extra_compile_args["nvcc"] = extra_args + [
+            "-D__CUDA_NO_HALF_OPERATORS__",
+            "-D__CUDA_NO_HALF_CONVERSIONS__",
+            "-D__CUDA_NO_HALF2_OPERATORS__",
         ]
         sources += sources_cuda
     else:
-        print('Compiling {} without CUDA'.format(name))
+        print("Compiling {} without CUDA".format(name))
         extension = CppExtension
         # raise EnvironmentError('CUDA is required to compile MMDetection!')
 
     return extension(
-        name='{}.{}'.format(module, name),
-        sources=[os.path.join(*module.split('.'), p) for p in sources],
+        name="{}.{}".format(module, name),
+        sources=[os.path.join(*module.split("."), p) for p in sources],
         include_dirs=extra_include_path,
         define_macros=define_macros,
-        extra_compile_args=extra_compile_args)
+        extra_compile_args=extra_compile_args,
+    )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # add_mim_extention()
     setup(
-        name='OpenOccupancy',
-        version='0.0',
-        description=("OpenOccupancy: A Large Scale Benchmark for Surrounding Semantic Occupancy Perception"),
-        author='OpenOccupancy Contributors',
-        author_email='wangxiaofeng2020@ia.ac.cn',
-        keywords='Occupancy Perception',
+        name="OpenOccupancy",
+        version="0.0",
+        description=(
+            "OpenOccupancy: A Large Scale Benchmark for Surrounding Semantic Occupancy Perception"
+        ),
+        author="OpenOccupancy Contributors",
+        author_email="wangxiaofeng2020@ia.ac.cn",
+        keywords="Occupancy Perception",
         packages=find_packages(),
         include_package_data=True,
-        package_data={'projects.occ_plugin.ops': ['*/*.so']},
+        package_data={"projects.occ_plugin.ops": ["*/*.so"]},
         classifiers=[
             "Development Status :: 4 - Beta",
             "License :: OSI Approved :: Apache Software License",
@@ -61,7 +59,6 @@ if __name__ == '__main__':
             "Programming Language :: Python :: 3.7",
         ],
         license="Apache License 2.0",
-
         ext_modules=[
             make_cuda_ext(
                 name="occ_pool_ext",
@@ -69,6 +66,8 @@ if __name__ == '__main__':
                 sources=[
                     "src/occ_pool.cpp",
                     "src/occ_pool_cuda.cu",
-                ]),
+                ],
+            ),
         ],
-        cmdclass={'build_ext': BuildExtension})
+        cmdclass={"build_ext": BuildExtension},
+    )
